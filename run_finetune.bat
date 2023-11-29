91809f14e6bcd17b91d881f182726e880f3ca5bf
AnnyOrange <99806202+AnnyOrange@users.noreply.github.com>
2023-11-28 21:36:01 -0800

    Add files via upload

--- run_finetune.bat
+++ run_finetune.bat
@@ -1,5 +1,5 @@
 set MODEL_ID=timbrooks/instruct-pix2pix
-set DATASET_ID=annyorange/colorized-dataset
+set DATASET_ID=annyorange/colorized_people-dataset
 set OUTPUT_DIR=colorization-finetuned
 
 accelerate launch --mixed_precision="fp16" finetune_instruct_pix2pix.py ^
@@ -8,13 +8,13 @@ accelerate launch --mixed_precision="fp16" finetune_instruct_pix2pix.py ^
   --use_ema ^
   --enable_xformers_memory_efficient_attention ^
   --resolution=256 --random_flip ^
-  --train_batch_size=2 --gradient_accumulation_steps=4 --gradient_checkpointing ^
-  --max_train_steps=50 ^
-  --checkpointing_steps=5000 --checkpoints_total_limit=2 ^
+  --train_batch_size=4 --gradient_accumulation_steps=4 --gradient_checkpointing ^
+  --max_train_steps=5 ^
+  --checkpointing_steps=15 --checkpoints_total_limit=2 ^
   --learning_rate=5e-06 --lr_warmup_steps=0 ^
   --mixed_precision=fp16 ^
-  --val_image_url="https://raw.githubusercontent.com/AllenAnZifeng/DeepLearning282/main/test_data/12.jpg" ^
-  --val_gt_url="https://raw.githubusercontent.com/AllenAnZifeng/DeepLearning282/main/test_data/12-g.jpg" ^
+  --val_image_url="https://raw.githubusercontent.com/AllenAnZifeng/DeepLearning282/main/test_data/0.jpg" ^
+  --val_gt_url="https://raw.githubusercontent.com/AllenAnZifeng/DeepLearning282/main/test_data/0-g.jpg" ^
   --validation_prompt="Colorized the image." ^
   --seed=42 ^
   --output_dir=%OUTPUT_DIR% ^
